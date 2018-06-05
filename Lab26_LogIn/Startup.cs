7ef0103b6f1fb2a7412241929e557a603067a1aa
Sarah <Ms.sarahelizabethb@gmail.com>
2018-06-05 15:22:36 -0400

    first commit

---
+++ Lab26_LogIn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab26_LogIn.Startup))]
+namespace Lab26_LogIn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
